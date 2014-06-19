3a2ce887766f022179ad581ffebd1791e4df5539
NathanDeMaria <naterstd@gmail.com>
2014-06-18 21:38:55 -0400

    Reworked the function to avoid copying dataset (actually makes it faster)

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,8 @@
-# Current directory must be UCI HAR Dataset/
+# Notes: ####
+	# Current directory must be UCI HAR Dataset/
 	# unzipped from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+# 1. Merge datasets ####
 
 labels <- read.table('activity_labels.txt')
 features <- read.table('features.txt', stringsAsFactors = F)
@@ -11,9 +14,11 @@ read_dataset <- function(directory) {
 	y <- read.table(paste0(directory, '/y_', directory, '.txt'))
 	y[,1] <- labels[y[,1],2]
 	
-	d <- cbind(s, X, y)
-	colnames(d) <- c('subject', features[,2], 'activity')
-	d
+	cbind(s, X, y)
 }
 
 data <- rbind(read_dataset('test'), read_dataset('train'))
+colnames(data) <- c('subject', features[,2], 'activity')
+
+
+# 2. Extract mean and sd measurements only ####
