e4ef09b0b782229c42c7a24ba92fb228a2b7dd99
NathanDeMaria <naterstd@gmail.com>
2014-06-18 22:18:24 -0400

    Commented where steps 1-4 happen

--- run_analysis.R
+++ run_analysis.R
@@ -12,15 +12,18 @@ read_dataset <- function(directory) {
 	s <- read.table(paste0(directory, '/subject_', directory, '.txt'))
 	X <- read.table(paste0(directory, '/X_', directory, '.txt'))
 	y <- read.table(paste0(directory, '/y_', directory, '.txt'))
+	
+	# 3. Appropriately labels the data set with descriptive variable names.
 	y[,1] <- labels[y[,1],2]
 	
 	cbind(s, X, y)
 }
 
 data <- rbind(read_dataset('test'), read_dataset('train'))
+
+# 4. Appropriately labels the data set with descriptive variable names. 
 colnames(data) <- c('subject', features[,2], 'activity')
 rm('features', 'labels')
 
 # 2. Extract mean and sd measurements only ####
-data <- data[,grep('(mean|std)\\(\\)', colnames(data))]
-
+data <- data[,c(1, grep('(mean|std)\\(\\)', colnames(data)), dim(data)[2])]
